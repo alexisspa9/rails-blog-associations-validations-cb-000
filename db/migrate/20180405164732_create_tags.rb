791a879ce2bb4902a283bd50496c39f2730527d8
Alexander Spanos <alexisspa9@gmail.com>
2018-04-05 16:51:36 +0000

    automatically backed up by learn

--- db/migrate/20180405164732_create_tags.rb
+++ db/migrate/20180405164732_create_tags.rb
@@ -2,8 +2,7 @@ class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
       t.string :name
-
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
