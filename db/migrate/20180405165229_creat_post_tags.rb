26f123c6b16040dfeaaa84d4e3f56892aa686cec
Alexander Spanos <alexisspa9@gmail.com>
2018-04-05 16:52:34 +0000

    automatically backed up by learn

--- db/migrate/20180405165229_creat_post_tags.rb
+++ db/migrate/20180405165229_creat_post_tags.rb
@@ -1,4 +1,9 @@
-class CreatPostTags < ActiveRecord::Migration
+class CreatePostTags < ActiveRecord::Migration
   def change
+    create_table :post_tags do |t|
+      t.integer :post_id
+      t.integer :tag_id
+      t.timestamps
+    end
   end
 end
